ab1f7e0a67a58d9ec18d47c23ccbaadade6a76d5
jbt017 <jbt017@gmail.com>
2019-05-01 11:03:40 -0500

    GUI formatting edit

--- smartpot.py
+++ smartpot.py
@@ -5,6 +5,7 @@
 
 from Tkinter import *
 
+# class for creating the window
 class GUI(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
@@ -12,29 +13,33 @@ class GUI(Frame):
 
     def setupGUI(self):
         a1 = Label(self.master, text='Photo Level')
-        a1.grid(row = 1, column = 1, columnspan = 2, sticky="W")
+        a1.grid(row = 1, column = 1, columnspan = 2, sticky="E" + "W")
 
         a2 = Label(self.master, text='Moisture Level')
         a2.grid(row= 1, column = 3, columnspan = 2, sticky = 'W')
 
-        b1 = Label(self.master, text = 'Current')
-        b1.grid(row = 2, column = 1, columnspan = 2, sticky = 'W')
+        b1 = Label(self.master, text = '0')
+        b1.grid(row = 2, column = 1, columnspan = 2, sticky = "E" + 'W')
 
-        b2 = Label(self.master, text = 'Current')
-        b2.grid(row = 2, column = 3, columnspan = 2, sticky = 'W')
+        b2 = Label(self.master, text = '0')
+        b2.grid(row = 2, column = 3, columnspan = 2, sticky = 'E'+'W')
 
         a3 = Scale(self.master, orient=HORIZONTAL, length = 100, from_=1.0, to=100)
-        a3.grid(row = 3, column = 1, columnspan = 2, sticky = 'W')
+        a3.grid(row = 3, column = 1, columnspan = 2, sticky ='E'+'W')
 
         b3 = Scale(self.master, orient=HORIZONTAL, length = 100, from_=1.0, to=100)
-        b3.grid(row = 3, column = 3, columnspan = 2, sticky = 'W')
+        b3.grid(row = 3, column = 3, columnspan = 2, sticky ='E'+'W')
 
+    
+    #function for changing listed moisture sensor value
     def currentMoisture(self):
         pass
 
+    #function for changing listed moisture sensor value
     def currentPhoto(self):
         pass
 
+#create new instance of GUI class
 window = Tk()
 t = GUI(window)
 t.setupGUI()

d324b77dc4c9eedeb396cee55dbeab5f14c7474a
jbt017 <jbt017@gmail.com>
2019-05-07 13:56:35 -0500

    GUI updates, added most home page monitors, no logic

--- smartpot.py
+++ smartpot.py
@@ -5,6 +5,11 @@
 
 import Tkinter as tk
 import random
+import matplotlib
+matplotlib.use("TkAgg")
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2TkAgg)
+from matplotlib.figure import Figure
+import datetime
 #from time import *
 #import spidev
 
@@ -18,6 +23,8 @@ class SmartPot(tk.Tk):
 
         # variable containing the frame
         container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand=True)
 
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
@@ -45,70 +52,114 @@ class HomePage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text = "Smart Pot")
-        label.grid(row=0, column=0, sticky='nsew')
+        label = tk.Label(self, text = "Smart Pot", font='Arial, 16')
+        label.grid(row=0, column=0, columnspan=5, sticky='nsew')
 
-        moistLabel = tk.Label(self, text = "Moisture Level")
+        moistLabel = tk.Label(self, text = "Moisture Level", padx=10)
         moistLabel.grid(row=1, column=0, sticky='nsew')
 
-        photoLabel = tk.Label(self, text = "Light Level")
+        photoLabel = tk.Label(self, text = "Light Level", padx=20)
         photoLabel.grid(row=1, column=1, sticky='nsew')
 
-        previousdayLabel = tk.Label(self, text = "Previous Day Plot")
-        previousdayLabel.grid(row=2, column=1, sticky='nsew')
+        moistValueCurrent = tk.Label(self, text="0%", background= 'white', font='Arial, 12', padx=10, pady=5)
+        moistValueCurrent.config(relief='sunken')
+        moistValueCurrent.grid(row=2, column=0, sticky="nsew")
 
-        sevendayLabel = tk.Label(self, text = "Seven Day Plot")
-        sevendayLabel.grid(row=2, column=2, sticky='nsew')
+        photoValueCurrent = tk.Label(self, text="0 lux", background= 'white', font='Arial, 12', padx=10, pady=5)
+        photoValueCurrent.config(relief='sunken')
+        photoValueCurrent.grid(row=2, column=1, sticky="nsew")
+
+        desiredLabel = tk.Label(self, text="Desired Values", padx=10)
+        desiredLabel.grid(row=3, column=0, columnspan=2, sticky='sew')
+
+        moistValueDesired = tk.Label(self, text="0%", background= 'white', font='Arial, 12', padx=10, pady=5)
+        moistValueDesired.config(relief='sunken')
+        moistValueDesired.grid(row=4, column=0, sticky="nsew")
+
+        photoValueDesired = tk.Label(self, text="0 lux", background= 'white', font='Arial, 12', padx=10, pady=5)
+        photoValueDesired.config(relief='sunken')
+        photoValueDesired.grid(row=4, column=1, sticky="nsew")
+
+        photoHeartbeat = tk.Label(self, text = "Photo Monitor")
+        photoHeartbeat.grid(row=1, column=3, sticky='nsew')
+
+        moistHeartbeat = tk.Label(self, text = "Moisture Monitor")
+        moistHeartbeat.grid(row=1, column=4, sticky='nsew')
+
+
+        # test graph values
+        f = Figure(figsize=(5,5), dpi=100)
+        a = f.add_subplot(111)
+        a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
+
+        # basic plot funtions for embedding a photo and moisture monitor graph
+        photoplot = FigureCanvasTkAgg(f, self)
+        photoplot.draw()
+        photoplot.get_tk_widget().grid(row=2, rowspan=6, column=3, sticky="nsew", padx=10, pady=10)
+
+        moistplot = FigureCanvasTkAgg(f, self)
+        moistplot.draw()
+        moistplot.get_tk_widget().grid(row=2, rowspan=6, column=4, sticky="nsew", padx=10, pady=10)
    
     
-    #function for changing listed moisture sensor value
-    def currentMoisture(self, channel):
-        moisture_level = 0
-        if Demo == True:
-            moisture_level = random.randint(50, 65)
-        else:
-            #acutal sensor calculation    
-            # spi = spidev.SpiDev()
-            # spi.open(0,0)
-            # spi.max_speed_hz = 250000
 
-            # assert 0 <= channel <= 1, 'ADC channel must be 0 or 1.'
-
-            # if channel:
-            #     cbyte = 0b11000000
-            # else:
-            #     cbyte = 0b10000000
-
-            # r = spi.xfer2([1,cbyte,0])
-            # return ((r[1] & 31) << 6) + (r[2] >> 2)
-
-            # try:
-            #     while True:
-            #         channel = 0
-            #         channeldata = poll_sensor(channel)
-
-
-            #         voltage = round(((channeldata * 3300)/1024),0)
-            #         moisture_level = round(voltage/280)
-            #         #print moisture_level
-
-            #         sleep(2)
-
-            # finally:
-            #     spi.close()
-            #     #print "/n All cleaned up."
-        return moisture_level
 
     #function for changing listed moisture sensor value
-    def currentPhoto(self):
-        photo_level = 0
-        if Demo == True:
-            # demo number generation
-            photo_level = random.randint(0, 1000)
-        else:
-            # actual sensor calculation
+def currentMoisture(self, channel):
+    datetime_current = datetime.datetime.now()
+    if Demo == True:
+        moisture_level = random.randint(50, 65)
+        poll_update = {datetime_current: moisture_level}
+        return poll_update
+    
+    else:
+        pass
+        #acutal sensor calculation    
+        # spi = spidev.SpiDev()
+        # spi.open(0,0)
+        # spi.max_speed_hz = 250000
 
+        # assert 0 <= channel <= 1, 'ADC channel must be 0 or 1.'
+
+        # if channel:
+        #     cbyte = 0b11000000
+        # else:
+        #     cbyte = 0b10000000
+
+        # r = spi.xfer2([1,cbyte,0])
+        # return ((r[1] & 31) << 6) + (r[2] >> 2)
+
+        # try:
+        #     while True:
+        #         channel = 0
+        #         channeldata = poll_sensor(channel)
+
+
+        #         voltage = round(((channeldata * 3300)/1024),0)
+        #         moisture_level = round(voltage/280)
+        #         #print moisture_level
+
+        #         sleep(2)
+
+        # finally:
+        #     spi.close()
+        #     #print "/n All cleaned up."
+        #return moisture_level
+
+            #function for changing listed moisture sensor value
+
+
+def currentPhoto(self):
+    photo_level = 0
+    if Demo == True:
+        # demo number generation
+        photo_level = random.randint(0, 1000)
         return photo_level
+    else:
+        pass
+        # actual sensor calculation
+
+        #return photo_level
 
 #create new instance of GUI class
 window = SmartPot()

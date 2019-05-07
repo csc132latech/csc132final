0afa1af65152a6610566e15d075ba5f487f6562d
jbt017 <jbt017@gmail.com>
2019-05-07 15:22:33 -0500

    fixed resolution

--- smartpot.py
+++ smartpot.py
@@ -10,13 +10,13 @@ matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2TkAgg)
 from matplotlib.figure import Figure
 import datetime
-from time import sleep
-import spidev
+# from time import sleep
+# import spidev
 
    
-spi = spidev.SpiDev()
-spi.open(0,0)
-spi.max_speed_hz = 250000
+# spi = spidev.SpiDev()
+# spi.open(0,0)
+# spi.max_speed_hz = 250000
 
 Demo = True # Use True to activate elements for testing without input data
 
@@ -27,7 +27,7 @@ class SmartPot(tk.Tk):
         tk.Tk.__init__(self, *args, **kwargs)
 
         # variable containing the frame
-        container = tk.Frame(self)
+        container = tk.Frame(self, width=800, height=640)
 
         container.pack(side="top", fill="both", expand=True)
 
@@ -117,33 +117,33 @@ def currentMoisture(channel):
         poll_update = {datetime_current: moisture_level}
         return poll_update
     
-    else:
-        assert 0 <= channel <= 1, 'ADC channel must be 0 or 1.'
+    # else:
+    #     assert 0 <= channel <= 1, 'ADC channel must be 0 or 1.'
 
-        if channel:
-            cbyte = 0b11000000
-        else:
-            cbyte = 0b10000000
+    #     if channel:
+    #         cbyte = 0b11000000
+    #     else:
+    #         cbyte = 0b10000000
 
-        r = spi.xfer2([1,cbyte,0])
-        return ((r[1] & 31) << 6) + (r[2] >> 2)
+    #     r = spi.xfer2([1,cbyte,0])
+    #     return ((r[1] & 31) << 6) + (r[2] >> 2)
 
-        try:    
-            channel = 0
-            channeldata = poll_sensor(channel)
+    #     try:    
+    #         channel = 0
+    #         channeldata = poll_sensor(channel)
 
 
-            voltage = round(((channeldata * 3300)/1024),0)
-            moisture_level = round(voltage/280)
-            #print moisture_level
+    #         voltage = round(((channeldata * 3300)/1024),0)
+    #         moisture_level = round(voltage/280)
+    #         #print moisture_level
 
-            sleep(2)
+    #         sleep(2)
 
-        finally:
-            spi.close()
-            #print "/n All cleaned up."
-        moisture_level = moisture_level * 10
-        return moisture_level
+    #     finally:
+    #         spi.close()
+    #         #print "/n All cleaned up."
+    #     moisture_level = moisture_level * 10
+    #     return moisture_level
 
             #function for changing listed moisture sensor value
 
